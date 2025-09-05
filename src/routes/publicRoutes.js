28e71fc6ec8cea7fa7983ca46852c35f2f1a7c28
leoLUIGY <luigyleonidas@gmail.com>
2025-09-05 13:47:11 -0300

    nova alteração

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -11,7 +11,7 @@ server.post('/cadastrar', async (req, res) => {
 
     if (emailExiste(email)) {
         const status = 401;
-        const message = 'E-mail já foi utilizado!';
+        const message = 'E-mail já esta em uso!';
         return res.status(status).json({ status, message });
 
     }
